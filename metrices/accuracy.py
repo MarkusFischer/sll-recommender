25d12c4b6f2f6659e77b6101e718d9a3cac28519
Markus Fischer <markus.fischer@uni-jena.de>
2019-07-06 15:14:12 +0200

    implemented three basic accuracy metrics

---
+++ metrices/accuracy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def mse(y, y_hat):
+    error = y - y_hat
+    return np.sum(error*error)/len(error)
+
+def rmse(y, y_hat):
+    return np.sqrt(mse(y, y_hat))
+
+def mae(y, y_hat):
+    error = y - y_hat
+    return np.sum(np.abs(error))/len(error)
