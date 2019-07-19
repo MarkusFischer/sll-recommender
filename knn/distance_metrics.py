8338ebb1c849e39e37c1c2ed6930b4cd619d2aa8
Markus Fischer <markus.fischer@uni-jena.de>
2019-07-19 21:47:48 +0200

    similarity matrices for rows work

--- knn/distance_metrics.py
+++ knn/distance_metrics.py
@@ -5,21 +5,29 @@ import numpy as np
 from utility.matrices import make_rows_mean_free
 
 
-class PearsonSimiliarityMatrix:
+class SimiliarityMatrix:
     def __init__(self,
                  data_matrix,
                  axis=0,
+                 method="pearson",
                  verbose=False):
         self.data = data_matrix
         if axis != 0 and axis != 1:
             raise Exception("Wrong value for axis")
         self.axis = axis
         self.similarity = np.eye(data_matrix.shape[axis])
+        self.mean = np.zeros((data_matrix.shape[axis],1))
         self.verbose = verbose
+        if method.lower() != "pearson" and method.lower() != "cosine":
+            raise Exception("Uncommon method!")
+        self.method = method.lower()
 
     def save(self, filename):
         with open(filename, "wb") as file:
             pickle.dump(self.similarity, file)
+        with open("mean_" + filename, "wb") as file:
+            pickle.dump(self.mean, file)
+
 
     def fit(self):
         row_sum = np.sum(self.data, axis=1-self.axis)
@@ -27,6 +35,7 @@ class PearsonSimiliarityMatrix:
         data_bin[self.data != 0] = 1
         entry_count = np.sum(data_bin, axis=1-self.axis)
         mean = (row_sum / entry_count).reshape(-1,1)
+        self.mean = mean
         if self.axis == 0:
             mean_free_data = self.data - mean
         else:
@@ -34,15 +43,22 @@ class PearsonSimiliarityMatrix:
         for u in range(0,self.data.shape[self.axis]):
             print(f"Running {u} of {self.data.shape[self.axis]}")
             for v in range(u + 1, self.data.shape[self.axis]):
-                if self.axis == 0:
-                    items_u = mean_free_data[u,:][data_bin[u,:] == data_bin[v,:]]
-                    items_v = mean_free_data[v, :][data_bin[u,:] == data_bin[v,:]]
-                    self.similarity[u,v] = np.sum(items_u*items_v)/(np.sqrt(np.sum(items_u**2))*np.sqrt(np.sum(items_v**2)))
+                if self.method == "pearson":
+                    if self.axis == 0:
+                        items_u = mean_free_data[u,:][data_bin[u,:] == data_bin[v,:]]
+                        items_v = mean_free_data[v, :][data_bin[u,:] == data_bin[v,:]]
+                    else:
+                        items_u = mean_free_data[u,:][data_bin[:,u] == data_bin[:,v]]
+                        items_v = mean_free_data[v,:][data_bin[:,u] == data_bin[:,v]]
                 else:
-                    items_u = mean_free_data[:,u][data_bin[:,u] == data_bin[:,v]]
-                    items_v = mean_free_data[:,v][data_bin[:,u] == data_bin[:,v]]
-                    self.similarity[u, v] = np.sum(items_u * items_v) / (
-                                np.sqrt(np.sum(items_u ** 2)) * np.sqrt(np.sum(items_v ** 2)))
+                    if self.axis == 0:
+                        items_u = self.data[u,:][data_bin[u,:] == data_bin[v,:]]
+                        items_v = self.data[v, :][data_bin[u,:] == data_bin[v,:]]
+                    else:
+                        items_u = self.data[:,u][data_bin[:,u] == data_bin[:,v]]
+                        items_v = self.data[:,v][data_bin[:,u] == data_bin[:,v]]
+                self.similarity[u, v] = np.sum(items_u * items_v) / (
+                            np.sqrt(np.sum(items_u ** 2)) * np.sqrt(np.sum(items_v ** 2)))
         lower_indices = np.tril_indices(self.data.shape[self.axis],-1)
         self.similarity[lower_indices] = self.similarity.T[lower_indices]
 
@@ -89,5 +105,5 @@ if __name__ == "__main__":
     example_bin[example != 0] = 1
     entry_count = np.sum(example_bin, axis=1)
     mean = row_sum/entry_count
-    psm = PearsonSimiliarityMatrix(example)
+    psm = SimiliarityMatrix(example,axis=1,)
     psm.fit()
