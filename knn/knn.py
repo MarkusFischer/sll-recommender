221848f1c17548d92897563a21a7240375d0d488
Markus Fischer <markus.fischer@uni-jena.de>
2019-07-20 12:39:45 +0200

    row wise knn could work

--- knn/knn.py
+++ knn/knn.py
@@ -1,53 +1,49 @@
+import numbers
+
 import numpy as np
 
-from utility.matrices import make_rows_mean_free
-
+from knn.distance_metrics import SimiliarityMatrix
 
 class kNN:
-    def __init__(self, train, k, row_metric, column_metric, row_weight=1, col_weight=1,regression=False,
-                 col_mean_usage=False, row_mean_usage=True):
-        self.train_matrix = train
-        mean_free, mean = make_rows_mean_free(train)
-        self.train_matrix_mean_free = mean_free
-        self.mean = mean
+    def __init__(self,
+                 data,
+                 user_similarity,
+                 item_similarity,
+                 row_mean=None,
+                 k=3):
+        self.data = data
+
+        self.data_bin = np.zeros_like(data)
+        self.data_bin[data != 0] = 1
+
+        if user_similarity.shape[0] != user_similarity.shape[1] or user_similarity.shape[0] != data.shape[0]:
+            raise Exception("User similarity matrix is not a square matrix or dimension mismatch!")
+        self.user_similarity = user_similarity
+        if item_similarity.shape[0] != item_similarity.shape[1] or item_similarity.shape[0] != data.shape[1]:
+            raise Exception("Item similarity matrix is not a square matrix or dimension mismatch!")
+        self.item_similarity = item_similarity
+        if row_mean is not None:
+            self.row_mean = row_mean
+        else:
+            self.row_mean = np.zeros((data.shape[0],1))
+        self.data_mean_free = data - row_mean
+        if not isinstance(k, numbers.Number) or k <= 0:
+            raise Exception(f"K value {k} is to small or not a number!")#todo check if not int
         self.k = k
-        if (k <= 0):
-            raise Exception(f"K value {self.k} is to small")#todo check if not int
-        self.row_metric=row_metric
-        self.column_metric=column_metric
-        self.row_weight=row_weight
-        self.col_weight=col_weight
-        self.regression=regression
-        self.col_mean_usage=col_mean_usage
-        self.row_mean_usage=row_mean_usage
+
+
+    def classify(self,coords,axis=None):
+        if axis == 0:
+            sim = []
+            ratings = []
+            for (u,j) in coords[:,(0,1)]:#todo sieh nach ob die item j überhaupt bewertet haben
+                nearest = np.argpartition(self.user_similarity[u,:], -(self.k+1))[-(self.k+1):]
+                nearest = nearest[nearest != u]
+                sim.append(self.user_similarity[u,nearest])
+                ratings.append(self.data_mean_free[nearest,j])
+            sim = np.array(sim)
+            ratings = np.array(ratings)
+            return self.row_mean[coords[:,0]].T + (np.sum(sim*ratings,axis=1))/(np.sum(np.abs(sim),axis=1))
         pass
 
-    def classify(self,u,i,dir=None): #todo vectorize
-        if dir is None:
-            pass
-        elif dir == 0:
-            neighbourhood = {}
-            for j in range(0,self.train_matrix.shape[0]): #todo what happens when row is not given?
-                if j == u: #todo better way
-                    continue
-                if self.train_matrix[j, i] == 0:
-                    continue
-                if self.row_mean_usage:
-                    neighbourhood[j] = self.row_metric(u,j,self.train_matrix,self.mean)
-                else:
-                    neighbourhood[j] = self.row_metric(u, j, self.train_matrix)
-            sorted_neighbourhood = sorted(neighbourhood.items(), key=lambda item: item[1],reverse=True)
-            knearest = sorted_neighbourhood[:(self.k)]
-            if self.row_mean_usage:
-                s = np.empty((0,0))
-                sim = 0
-                for v in knearest:
-                    np.append(s, self.row_metric(u,v[0],self.train_matrix,self.mean)*self.train_matrix_mean_free[v[0],i])
-                    sim += np.abs(self.row_metric(u,v[0],self.train_matrix,self.mean))
-                return self.mean[u] + (np.sum(s)/sim)
-            else:
-                pass
-        elif dir == 1:
-            pass
-        else:
-            raise Exception(f"Argument value {dir} not supported for parameter dir")
+
--- playground.py
+++ playground.py
@@ -1,39 +1,15 @@
-import os
-
 import numpy as np
-import matplotlib.pyplot as plt
 
-from utility.matrices import convert_sparse_coo_to_full_matrix
+from knn.distance_metrics import SimiliarityMatrix
+from knn.knn import kNN
 
-A = np.array([[1, 2, 3, 0],
-              [0, 0, 2, 0],
-              [2, 1, 0, 0],
-              [0, 2, 2, 0]])
-
-values = np.unique(A)[1:] #0 nicht vergeben
-
-
-#fuer jeden Wert aus values:
-#hier beispielsweise 1
-A1 = np.full_like(A,1)
-A1[(A-1).nonzero()]=0
-
-A2 = np.full_like(A,2)
-A2[(A-2).nonzero()]=0
-
-A3 = np.full_like(A,3)
-A3[(A-3).nonzero()]=0
-
-#Anzahl der belegten Reihen mit Wert i
-row_count = np.sum(A2,axis=1)/2
-
-X_raw = np.genfromtxt(os.path.join("data", "train.csv"), delimiter=",", dtype=np.int)
-X_raw[:,2] += 1
-Xq = np.genfromtxt(os.path.join("data", "qualifying_blanc.csv"), delimiter=",", dtype=np.int)
-q = np.full((Xq.shape[0],1),7)
-X_raw_mat = convert_sparse_coo_to_full_matrix(X_raw)
-X_q_mat = convert_sparse_coo_to_full_matrix(np.column_stack((Xq, q)))
-res = X_raw_mat.toarray() + X_q_mat.toarray()
-plt.matshow(res)
-plt.colorbar()
-plt.show()
+example = np.array([[7, 6, 7, 4, 5, 4],
+                        [6, 7, 0, 4, 3, 4],
+                        [0, 3, 3, 1, 1, 0],
+                        [1, 2, 2, 3, 3, 4],
+                        [1, 0, 1, 2, 3, 3]])
+user_sim = SimiliarityMatrix(example, axis=0)
+user_sim.fit()
+item_sim = SimiliarityMatrix(example, axis=1)
+item_sim.fit()
+classificator = kNN(example, user_sim.similarity, item_sim.similarity, user_sim.mean,k=2)
