a77b9fc1aa75cc003faf982f24c6735c536edea7
Markus Fischer <markus.fischer@uni-jena.de>
2019-07-20 15:41:19 +0200

    and helper updated again

--- generate_sim.py
+++ generate_sim.py
@@ -17,11 +17,22 @@ X_train_raw[:,2] += 1
 data = convert_sparse_coo_to_full_matrix(X_train_raw).toarray()
 
 print("calculate pearson similarity matrix for user")
+pearson = SimiliarityMatrix(data,axis=0,verbose=True)
+pearson.fit()
+pearson.save("user_pearson_sim.pyc")
+
+print("calculate cosine similarity matrix for user")
+cosine = SimiliarityMatrix(data, axis=0,method="cosine",verbose=True)
+cosine.fit()
+cosine.save("user_cosine_sim.pyc")
+
+
+print("calculate pearson similarity matrix for item")
 pearson = SimiliarityMatrix(data,axis=1,verbose=True)
 pearson.fit()
 pearson.save("item_pearson_sim.pyc")
 
 print("calculate cosine similarity matrix for user")
-cosine = SimiliarityMatrix(data, axis=1,method="cosine",verbose=True)
+cosine = SimiliarityMatrix(data, axis=0,method="cosine",verbose=True)
 cosine.fit()
 cosine.save("item_cosine_sim.pyc")
