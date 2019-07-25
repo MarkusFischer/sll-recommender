f76e78931483677642fc55714048081c9f1727c5
Markus Fischer <markus.fischer@uni-jena.de>
2019-07-25 08:16:49 +0200

    code clean up

--- metrices/accuracy.py
+++ metrices/accuracy.py
@@ -5,8 +5,10 @@ def mse(y, y_hat):
     error = y - y_hat
     return np.sum(error*error)/len(error)
 
+
 def rmse(y, y_hat):
     return np.sqrt(mse(y, y_hat))
+
 
 def mae(y, y_hat):
     error = y - y_hat
--- utility/matrices.py
+++ utility/matrices.py
@@ -1,14 +1,5 @@
 from scipy import sparse
-import numpy as np
 
 
 def convert_sparse_coo_to_full_matrix(matrix):
     return sparse.coo_matrix((matrix[:,2], (matrix[:,0], matrix[:,1]))).tocsr()
-
-def make_rows_mean_free(matrix):
-    row_sums = matrix.sum(axis=1)
-    print(row_sums.shape)
-    nnz = matrix.getnnz(axis=1).reshape((-1,1))
-    nnz[nnz[:]==0] = 1
-    mean = row_sums/nnz.reshape((-1,1))
-    return (matrix - mean, mean)
